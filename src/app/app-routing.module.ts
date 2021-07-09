ea33cb709933b6499d338627186c69458138d584
LuongMinh13 <l.minh1995@yahoo.fr>
2021-07-09 20:54:13 +0200

    home reservation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChefComponent } from './chef/chef.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ReviewComponent } from './review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reservation', component: ReservationComponent},
+  {path: '', component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'chef', component: ChefComponent},
+  {path: 'review', component: ReviewComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
+import { ChefComponent } from './chef/chef.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ReservationComponent,
+    HomeComponent,
+    ChefComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
